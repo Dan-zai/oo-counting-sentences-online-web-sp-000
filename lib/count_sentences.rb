a6f078cbd64f6c18d2e8f55215f20f393ae4899f
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-13 08:01:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,26 @@ class String
 
   def sentence?
     self.end_with?(".")
+    true 
+  else
+    false
   end
+end
 
   def question?
     self.end_with?("?")
-
-  end
-
+    true 
+  else
+    false
+ end
+end
   def exclamation?
   self.end_with?("!")
-  end
-
+      true 
+  else
+    false
+ end
+end
   def count_sentences
     proper_sentences = self.split
     proper_sentences.length
